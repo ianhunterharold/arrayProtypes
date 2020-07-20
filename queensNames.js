5078b9bf4800b8cb62c3be8f5bcd823e9a5acc9e
Ian Harold <ianhunterharold@gmail.com>
2020-07-20 16:39:35 -0600

    queens sorted by name in alphabetical order

---
+++ queensNames.js
@@ -0,0 +1,215 @@
+  //1.  Write a function that returns an array of queen names in alphabetical order.
+
+// sortFirstNames()
+
+const sortQueensByNames = () => {
+ 
+    const names = queens.map(currentValue => {
+      return currentValue['name'];
+    })
+   
+   return names.sort();
+    
+  }
+  sortQueensByNames();
+
+
+const queens = [
+    {
+      "id": 55,
+      "name": "Lineysha Sparx",
+      "winner": false,
+      "missCongeniality": false,
+      "quote": "Don't worry, she thinks she's fashion but... She dresses like a Dracula",
+      "seasons": [
+        {
+          "seasonNumber": "5",
+          "id": 6,
+          "place": 9
+        }
+      ]
+    },
+    {
+      "id": 53,
+      "name": "Vivienne Pinay",
+      "winner": false,
+      "missCongeniality": false,
+      "quote": "Vivienne is all about glamour. My beauty is everything.",
+      "seasons": [
+        {
+          "seasonNumber": "5",
+          "id": 6,
+          "place": 11
+        }
+      ]
+    },
+    {
+      "id": 52,
+      "name": "Monica Beverly Hillz",
+      "winner": false,
+      "missCongeniality": false,
+      "quote": "I'm Monica Beverly Hillz with a z.",
+      "seasons": [
+        {
+          "seasonNumber": "5",
+          "id": 6,
+          "place": 12
+        }
+      ]
+    },
+    {
+      "id": 59,
+      "name": "Coco Montrese",
+      "winner": false,
+      "missCongeniality": false,
+      "quote": "I'm not joking bitch!",
+      "seasons": [
+        {
+          "seasonNumber": "5",
+          "id": 6,
+          "place": 5
+        }
+      ]
+    },
+    {
+      "id": 57,
+      "name": "Ivy Winters",
+      "winner": false,
+      "missCongeniality": true,
+      "quote": "Hey ladies! Oh my god!",
+      "seasons": [
+        {
+          "seasonNumber": "5",
+          "id": 6,
+          "place": 7
+        }
+      ]
+    },
+    {
+      "id": 61,
+      "name": "Roxxy Andrews",
+      "winner": false,
+      "missCongeniality": false,
+      "quote": "When life pulls you down, put on a shiny sequence gown",
+      "seasons": [
+        {
+          "seasonNumber": "5",
+          "id": 6,
+          "place": 3
+        }
+      ]
+    },
+    {
+      "id": 60,
+      "name": "Detox Icunt",
+      "winner": false,
+      "missCongeniality": false,
+      "quote": "I have HAD IT... officially!",
+      "seasons": [
+        {
+          "seasonNumber": "5",
+          "id": 6,
+          "place": 4
+        }
+      ]
+    },
+    {
+      "id": 58,
+      "name": "Alyssa Edwards",
+      "winner": false,
+      "missCongeniality": false,
+      "quote": "BACKROLLS?!?!",
+      "seasons": [
+        {
+          "seasonNumber": "5",
+          "id": 6,
+          "place": 6
+        }
+      ]
+    },
+    {
+      "id": 51,
+      "name": "Serena Cha Cha",
+      "winner": false,
+      "missCongeniality": false,
+      "quote": "Does it bother you I keep saying I'm 21?!",
+      "seasons": [
+        {
+          "seasonNumber": "5",
+          "id": 6,
+          "place": 13
+        }
+      ]
+    },
+    {
+      "id": 54,
+      "name": "Honey Mahogany",
+      "winner": false,
+      "missCongeniality": false,
+      "quote": "Hey sisters!",
+      "seasons": [
+        {
+          "seasonNumber": "5",
+          "id": 6,
+          "place": 10
+        }
+      ]
+    },
+    {
+      "id": 50,
+      "name": "Penny Tration",
+      "winner": false,
+      "missCongeniality": false,
+      "quote": "Hello boys!",
+      "seasons": [
+        {
+          "seasonNumber": "5",
+          "id": 6,
+          "place": 14
+        }
+      ]
+    },
+    {
+      "id": 56,
+      "name": "Jade Jolie",
+      "winner": false,
+      "missCongeniality": false,
+      "quote": "Serving up fish, tuna on a platter.",
+      "seasons": [
+        {
+          "seasonNumber": "5",
+          "id": 6,
+          "place": 8
+        }
+      ]
+    },
+    {
+      "id": 63,
+      "name": "Jinkx Monsoon",
+      "winner": true,
+      "missCongeniality": false,
+      "quote": "I am Seattle's premiere Jewish narcoleptic drag queen, thank you.",
+      "seasons": [
+        {
+          "seasonNumber": "5",
+          "id": 6,
+          "place": 1
+        }
+      ]
+    },
+    {
+      "id": 62,
+      "name": "Alaska Thunderfuck 5000",
+      "winner": false,
+      "missCongeniality": false,
+      "quote": "I'll send you 10,000 dollars via PayPal if you let me stay in this competition",
+      "seasons": [
+        {
+          "seasonNumber": "5",
+          "id": 6,
+          "place": 2
+        }
+      ]
+    }
+  ]
+
