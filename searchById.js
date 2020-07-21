3828e9985ee179e705f0fd765ee5594e0a406a40
Ian Harold <ianhunterharold@gmail.com>
2020-07-21 09:34:27 -0600

    refactored .find into one linter for readability and changed to dot notation

--- searchById.js
+++ searchById.js
@@ -1,14 +1,11 @@
 
 // 3. Write a fuction that takes in an id and returns the string "[Queen's Name] placed [fifth, eleventh, ect...] in Season [Season number] of RuPaul's Drag Race."
 
-findQueenById(58)
-
 const findQueenById = (id) => {
-  const findById = queens.find(queen =>{
-    return queen['id'] === id 
-  })
-
-    return `${findById['name']} placed ${findById['seasons'][0]['place']} in Season ${findById['seasons'][0]['seasonNumber']} of RuPaul's Drag Race.`
+  
+  const findById = queens.find( queen => queen.id === id )
+   
+    return `${findById.name} placed ${findById.seasons[0].place} in Season ${findById.seasons[0].seasonNumber} of RuPaul's Drag Race.`
 
 }
 
