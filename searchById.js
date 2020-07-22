ec3955750f1f82223c689960c491c89a1a8c6649
Ian Harold <ianhunterharold@gmail.com>
2020-07-22 09:28:09 -0600

    using restructuring to simplify string

--- searchById.js
+++ searchById.js
@@ -2,11 +2,12 @@
 // 3. Write a fuction that takes in an id and returns the string "[Queen's Name] placed [fifth, eleventh, ect...] in Season [Season number] of RuPaul's Drag Race."
 
 const findQueenById = (id) => {
-  
   const findById = queens.find( queen => queen.id === id )
-   
-    return `${findById.name} placed ${findById.seasons[0].place} in Season ${findById.seasons[0].seasonNumber} of RuPaul's Drag Race.`
+  
+  const { name, seasons } = findById;
+  const { place, seasonNumber } = seasons[0];
 
+    return `${name} placed ${place} in Season ${seasonNumber} of RuPaul's Drag Race.`
 }
 
 const queenId = 56;
