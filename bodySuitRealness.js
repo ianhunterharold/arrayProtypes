d3c157d86f4dc652c6034dda4216122be7ec002a
Ian Harold <ianhunterharold@gmail.com>
2020-07-23 10:00:53 -0600

    bodysuit questions

---
+++ bodySuitRealness.js
@@ -0,0 +1,30 @@
+
+// create a list of all of beyonces songs where she is wearing a bodysuit or bodysuit is part of the video theme
+
+// const bodysuitSongs = () => {
+//   return beyonce.hits.reduce((acc,song) =>{
+
+//     if (song.signatureLook.includes('bodysuit')){
+//       acc.push(song.title)
+//     }
+
+//     if (song.videoTheme.includes('bodysuit')){
+//       acc.push(song.title)
+//     }
+     
+//     // refactor code above 
+//     return acc.sort();
+
+
+//   },[])
+// }
+// bodysuitSongs()
+
+
+// give me a song where Beyonce is not wearing a bodysuit
+
+// const notWearingBodysuit = () => {
+//   const songs = beyonce.hits.map(song => song.title).sort();
+//   return songs.find(song => song !== bodysuitSongs )
+// }
+// notWearingBodysuit();
