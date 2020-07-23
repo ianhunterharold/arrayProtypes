3c354be73a5db0c5e86b273869159d9ee46f9cc5
Ian Harold <ianhunterharold@gmail.com>
2020-07-23 15:58:50 -0600

    working solution

--- uniqueHairstyles.js
+++ uniqueHairstyles.js
@@ -1,18 +1,42 @@
+  // create an array of beyonces hairstyles without repeats
+
 const uniqueHairStyles = () => {
-    const hair = beyonce.hits.reduce((acc,cur) => {
-      acc += cur.hair
-      return acc
-    },"")
-    const split = hair.split(",");
-    const uniqueHair = new Set();
+  const hair = beyonce.hits.reduce((acc,cur) => {
+    acc += cur.hair
+    return acc
+  },"")
+  const split = hair.split(",");
+  const uniqueHair = new Set();
+
+  const addingToSet = split.forEach(hair => uniqueHair.add(hair));
   
-    const addingToSet = split.forEach(hair => uniqueHair.add(hair));
-    
-    return Array.from(uniqueHair); 
-    
-    //refactor soon
+  return Array.from(uniqueHair); 
   
-    //find unique hairvalues
+  //refactor soon
+  //find unique hairvalues
+  
+}
+uniqueHairStyles()
+
+// create an object where the keys are hairstyles and the value is the frequency of that hairstyle
+
+// {"hairstyles":count}
+
+const hairStylesCount = () => {
+
+    const allHair =  beyonce.hits.reduce((acc,cur) => {
+      acc.push(cur.hair)
+      return acc.flat() 
+    },[])
     
+    return allHair.reduce((acc,cur) => {
+      if (acc[cur]){
+        acc[cur] += 1;
+      } else{
+        acc[cur] = 1;
+      }
+      return acc;
+    },{})
+  
   }
-  uniqueHairStyles()
+  hairStylesCount()
